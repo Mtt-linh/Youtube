bdcbe2138de288aa1db171315dfab84ae5f1d4ec
Mtt-linh <63852165+Mtt-linh@users.noreply.github.com>
2020-08-31 11:43:09 +0700

    Add project files.

---
+++ Youtube/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using Youtube.Models;
+using static Youtube.Models.Video;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Youtube
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+          
+        }
+        public async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                RootObject myPage = await Video.GetPaper();
+                string image = String.Format(myPage.image);
+                ImageResult.Source = new BitmapImage(new Uri(image, UriKind.Absolute));
+                DateResult.Text = myPage.date;
+                TitleResult.Text = myPage.title;
+                string description = myPage.content.description;
+                ContentResult1.Text = description;
+
+
+
+            }
+            catch (Exception ex)
+            {
+              
+
+            }
+        }
+
+    }
+}
